da626ec63d40be029d33ef58e5e2f127828be08f
Ignacio Vargas <ignaevc@gmail.com>
2021-06-24 01:53:07 -0600

    Added constants file

---
+++ lib/RequestConstants.h
@@ -0,0 +1,11 @@
+#ifndef REQUEST_CONSTANTS_H
+#define REQUEST_CONSTANTS_H
+
+const int CLOSE             = 0;
+const int SAVE              = 1;
+const int WHOLE_BLOCK       = 2;
+const int FILE_FROM_NUM     = 3;
+const int METADATA_FROM_NUM = 4;
+const int FILE_AMOUNT       = 5;
+
+#endif
