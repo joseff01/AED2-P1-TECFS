47b0f65f2eb77ed47e59b9851bea278427f40db4
Ignacio Vargas <ignaevc@gmail.com>
2021-06-25 10:30:32 -0600

    Fixed huffman stuff so that it can be converted to and from json and used externally

--- lib/Huffman.cpp
+++ lib/Huffman.cpp
@@ -4,15 +4,17 @@
 #include "UtilityText.h"
 #include "Huffman.h"
 
-Huffman::Huffman(string text){
+Huffman::Huffman(string text)
+{
     huffmanTree(text);
 };
 
-void print_list(DecodeTreeNode* n)
+void print_list(DecodeTreeNode *n)
 {
     cout << "\nPrinting new list..." << endl;
-    while (n != nullptr) {
-        cout << n->letter << " " << n->code <<endl;
+    while (n != nullptr)
+    {
+        cout << n->letter << " " << n->code << endl;
         n = n->next;
     }
 }
@@ -23,40 +25,43 @@ void print_list(DecodeTreeNode* n)
  * @param root linked list with characters and encodded string that represents each character
  * @return encoded original string
  */
-string codedMessage(string og, DecodeTreeNode* root)
+string codedMessage(string og, DecodeTreeNode *root)
 {
     string str = "";
     cout << "Message coded:" << endl;
-    for(int i = 0; i<og.size();i++){
-        DecodeTreeNode* temp = root;
+    for (int i = 0; i < og.size(); i++)
+    {
+        DecodeTreeNode *temp = root;
         char ch = og[i];
-        while(root != nullptr)
+        while (root != nullptr)
         {
-            if(temp->letter==ch)
+            if (temp->letter == ch)
             {
-                str+= temp->code;
+                str += temp->code;
                 break;
             }
             temp = temp->next;
         }
     }
-    cout<< str<< endl;
+    cout << str << endl;
     return str;
-
 }
 /**
  * \brief auxiliar method that call decodeTemp. This method prints the decoded string
  * @param root Binary Tree to decode a recieved string
  * @param str encoded string given by the Hoffman Algorithm
  */
-void Huffman::decode(LeafNode* root, string str,string* decoded) {
+void Huffman::decode(LeafNode *root, string str, string *decoded)
+{
     int index = -1;
-    cout << "\nDecoded string is: \n";
+    cout << "\nDecoded string: ";
 
-    while (index < (int) str.size() - 1) {
-        decodeTemp(&root[0], index, str,decoded);
+    while (index < (int)str.size() - 1)
+    {
+        decodeTemp(&root[0], index, str, decoded);
     }
-    cout<<"\n"<<*decoded<<endl;
+    cout << *decoded << endl
+         << endl;
 }
 
 /**
@@ -65,24 +70,26 @@ void Huffman::decode(LeafNode* root, string str,string* decoded) {
  * @param index key that indicates iterative index for string
  * @param str encoded string given by the Hoffman Algorithm
  */
-void Huffman::decodeTemp(LeafNode* root, int &index, string str,string* decoded)
+void Huffman::decodeTemp(LeafNode *root, int &index, string str, string *decoded)
 {
-    if (root == nullptr) { //end of recursion
+    if (root == nullptr)
+    { //end of recursion
         return;
     }
 
     // found a leaf node
-    if (!root->left && !root->right){
+    if (!root->left && !root->right)
+    {
         //cout << root->letter;
         *decoded += root->letter;
         return;
     }
     index++;
 
-    if (str[index] =='0')
-        decodeTemp(root->left, index, str,decoded);
+    if (str[index] == '0')
+        decodeTemp(root->left, index, str, decoded);
     else
-        decodeTemp(root->right, index, str,decoded);
+        decodeTemp(root->right, index, str, decoded);
 }
 
 /**
@@ -91,7 +98,7 @@ void Huffman::decodeTemp(LeafNode* root, int &index, string str,string* decoded)
  * @param str recieved string
  * @param decode decoding linked list
  */
-void Huffman::encode(LeafNode* root, string str,DecodeTreeNode*& decode)
+void Huffman::encode(LeafNode *root, string str, DecodeTreeNode *&decode)
 {
     if (root == nullptr)
         return;
@@ -99,12 +106,14 @@ void Huffman::encode(LeafNode* root, string str,DecodeTreeNode*& decode)
     //found a leaf node
     if (root->left == nullptr && root->right == nullptr)
     {
-        if(decode->letter==NULL){ //first node of linked list
-            decode->code= str;
+        if (decode->letter == NULL)
+        { //first node of linked list
+            decode->code = str;
             decode->letter = root->letter;
         }
-        else{                     //New nodes
-            DecodeTreeNode* temp = new DecodeTreeNode();
+        else
+        { //New nodes
+            DecodeTreeNode *temp = new DecodeTreeNode();
             temp->letter = root->letter;
             temp->code = str;
             temp->next = decode;
@@ -119,21 +128,24 @@ void Huffman::encode(LeafNode* root, string str,DecodeTreeNode*& decode)
  * \brief HuffmanTree's constructor to encode a recieved message
  * @param text string recieved
  */
-void Huffman::huffmanTree(string text) {
+void Huffman::huffmanTree(string text)
+{
     string og = text;
     UtilityText *a = new UtilityText();
     ProcessNode *arrangedList = a->processText(text); //
 
     // Change ProcessNode to LeafNode
-    List<LeafNode *> root;                  // Array with nodes
-    while (arrangedList != nullptr) {
+    List<LeafNode *> root; // Array with nodes
+    while (arrangedList != nullptr)
+    {
         LeafNode *temp = new LeafNode(arrangedList->frequency, arrangedList->letter, nullptr, nullptr);
         root.push_back(temp);
         arrangedList = arrangedList->next;
     }
 
     //Build Tree
-    while (root.length() != 1) {            // Final length = 1
+    while (root.length() != 1)
+    { // Final length = 1
         LeafNode *left = root[0];
         root.erase(0);
         LeafNode *right = root[0];
@@ -142,15 +154,18 @@ void Huffman::huffmanTree(string text) {
 
         LeafNode *temp = new LeafNode(freq, '\0', left, right);
         root.push_back(temp);
-
     }
 
     auto *decoded = new DecodeTreeNode();
-    encode(root[0], "", decoded);   // Encodes original string
+    encode(root[0], "", decoded); // Encodes original string
 
     //print_list(decoded);
 
     string str = codedMessage(og, decoded); // Final coded string
+
+    this->encodedMsg = str;
+
+    this->decodificationTree = root[0];
 
     /*
     string x = "";
@@ -159,12 +174,76 @@ void Huffman::huffmanTree(string text) {
      */
 }
 
-
-
 LeafNode::LeafNode(int frequency, char letter, LeafNode *left, LeafNode *right)
 {
     this->frequency = frequency;
     this->letter = letter;
     this->left = left;
     this->right = right;
+}
+
+LeafNode *Huffman::getDecodeTree()
+{
+    return this->decodificationTree;
+}
+
+std::string Huffman::getEncodedMsg()
+{
+    return this->encodedMsg;
+}
+
+void to_json(json &j, const LeafNode &n)
+{
+    if (n.left == nullptr && n.right == nullptr)
+    {
+        j = json{{"Left", nullptr},
+                 {"Right", nullptr},
+                 {"Frequency", n.frequency},
+                 {"Letter", n.letter}};
+    }
+    else if (n.left == nullptr)
+    {
+        j = json{{"Left", nullptr},
+                 {"Right", *(n.right)},
+                 {"Frequency", n.frequency},
+                 {"Letter", n.letter}};
+    }
+    else if (n.right == nullptr)
+    {
+        j = json{{"Left", *(n.left)},
+                 {"Right", nullptr},
+                 {"Frequency", n.frequency},
+                 {"Letter", n.letter}};
+    }
+    else
+    {
+        j = json{{"Left", *(n.left)},
+                 {"Right", *(n.right)},
+                 {"Frequency", n.frequency},
+                 {"Letter", n.letter}};
+    }
+}
+
+void from_json(const json &j, LeafNode &n)
+{
+    n.frequency = j["Frequency"].get<int>();
+    n.letter = j["Letter"].get<char>();
+    if (j["Right"].is_null())
+    {
+        n.right = nullptr;
+    }
+    else
+    {
+        LeafNode *newRight = new LeafNode(j["Right"].get<LeafNode>());
+        n.right = newRight;
+    }
+    if (j["Left"].is_null())
+    {
+        n.left = nullptr;
+    }
+    else
+    {
+        LeafNode *newLeft = new LeafNode(j["Left"].get<LeafNode>());
+        n.left = newLeft;
+    }
 }
--- lib/Huffman.h
+++ lib/Huffman.h
@@ -5,24 +5,82 @@
 #ifndef AED2_P3_TECFS_HUFFMAN_H
 #define AED2_P3_TECFS_HUFFMAN_H
 
-
 #include <iostream>
 #include <vector>
 #include "List.h"
 
 using namespace std;
 
-class LeafNode{
+class LeafNode
+{
 public:
     int frequency;
     char letter;
     string code;
     LeafNode *left, *right;
-    LeafNode(int frequency,char letter,LeafNode *left, LeafNode *right);
-    LeafNode();
+    LeafNode(int frequency, char letter, LeafNode *left, LeafNode *right);
+    LeafNode() : left(nullptr), right(nullptr) {}
+    ~LeafNode()
+    {
+        if (right != nullptr)
+        {
+            delete right;
+        }
+        if (left != nullptr)
+        {
+            delete left;
+        }
+    }
+    LeafNode &operator=(LeafNode const &other)
+    {
+        this->frequency = other.frequency;
+        this->letter = other.letter;
+        // this->code = other.code; no sé si esto se usa
+        if (other.right == nullptr)
+        {
+            this->right = nullptr;
+        }
+        else
+        {
+            LeafNode *newRight = new LeafNode();
+            *newRight = *(other.right);
+            this->left = newRight;
+        }
+        if (other.left == nullptr)
+        {
+            this->left = nullptr;
+        }
+        else
+        {
+            LeafNode *newLeft = new LeafNode();
+            *newLeft = *(other.left);
+            this->left = newLeft;
+        }
+        return *this;
+    }
+    LeafNode(LeafNode const &other) : left(nullptr), right(nullptr)
+    {
+        this->frequency = other.frequency;
+        this->letter = other.letter;
+        if (other.right != nullptr)
+        {
+            this->right = new LeafNode();
+            *(this->right) = *other.right;
+        }
+        if (other.left != nullptr)
+        {
+            this->left = new LeafNode();
+            *(this->left) = *other.left;
+        }
+    }
 };
 
-class DecodeTreeNode{
+// json conversion
+void to_json(json &j, const LeafNode &n);
+void from_json(const json &j, LeafNode &n);
+
+class DecodeTreeNode
+{
 public:
     string code;
     char letter;
@@ -34,15 +92,16 @@ class Huffman
 {
 public:
     Huffman(string text);
-    void decode(LeafNode* root, string str,string* decoded);
-    void decodeTemp(LeafNode* root, int &index, string str, string* decoded);
-
+    static void decode(LeafNode *root, string str, string *decoded);
+    LeafNode *getDecodeTree();
+    std::string getEncodedMsg();
 
 private:
+    static void decodeTemp(LeafNode *root, int &index, string str, string *decoded);
     void huffmanTree(string text);
-    void encode(LeafNode* root, string str,DecodeTreeNode*& decode);
-
+    void encode(LeafNode *root, string str, DecodeTreeNode *&decode);
+    LeafNode *decodificationTree;
+    std::string encodedMsg;
 };
-
 
 #endif //AED2_P3_TECFS_HUFFMAN_H
