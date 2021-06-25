cb9bc56c6e0d65e84e51eccaa4c9f4342eb9eca6
Mariana <mariananavarro3022@gmail.com>
2021-06-25 02:10:04 -0600

    function clean up

--- ceSEARCH/cesearch_gui.cpp
+++ ceSEARCH/cesearch_gui.cpp
@@ -1,5 +1,6 @@
 #include "cesearch_gui.h"
 #include "ui_cesearch_gui.h"
+#include "../lib/List.h"
 
 ceSEARCH_GUI::ceSEARCH_GUI(QWidget *parent)
     : QMainWindow(parent)
@@ -16,6 +17,8 @@ ceSEARCH_GUI::~ceSEARCH_GUI()
 
 void ceSEARCH_GUI::on_pushButton_clicked()
 {
+    ui->listWidget->clear();
+    ui->plainTextEdit->clear();
     std::string str = ui->searchBar->text().toStdString();
     /*
      *send str to controllerNode
@@ -28,6 +31,15 @@ void ceSEARCH_GUI::on_pushButton_clicked()
     // Tomando en cuenta de que se supone que me mandan un List<string>
 
 
+    List<std::string> listStr; //given list by method
+
+    for(int i = 0; i<listStr.length();i++)
+    {
+        QString current = QString::fromStdString(listStr.at(i));
+        ui->listWidget->addItem(current);
+    }
+
+
 }
 
 void ceSEARCH_GUI::on_listWidget_itemClicked(QListWidgetItem *item)
@@ -36,12 +48,14 @@ void ceSEARCH_GUI::on_listWidget_itemClicked(QListWidgetItem *item)
     /*
      * ENVIAR str
      * esperar a recivir
+     * std::string  stringTemp= "ahjwdagdhkawhdalwjdhalwkdhalkjwdhalkjwdhaklwdjhalkwjhdalkdjhalkjwdhakjnskdjnamwdnasklhdnhjawhdlkajwhdakjhdaksndadnaijsndiauwdnlajdnwliaudadnahwdhadwadhawduhaidhawd";
+
      *
      */
 
     //Asumiendo que recive un string enorme
-    std::string  stringTemp= "ahjwdagdhkawhdalwjdhalwkdhalkjwdhalkjwdhaklwdjhalkwjhdalkdjhalkjwdhakjnskdjnamwdnasklhdnhjawhdlkajwhdakjhdaksndadnaijsndiauwdnlajdnwliaudadnahwdhadwadhawduhaidhawd";
 
+    std::string  stringTemp;
     QString qstr = QString::fromStdString(stringTemp);
     ui->plainTextEdit->setPlainText(qstr);
 
